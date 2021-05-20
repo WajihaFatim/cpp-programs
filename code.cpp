572f4ff075608f761e18518f913a6e2ef1096e4d
WajihaFatim <wajihafatima2003@gmail.com>
2021-05-20 11:29:34 -0400

    Template

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ps(x, y) fixed << setprecision(y) << x
+ #define BOOST  ios_base::sync_with_stdio(false);    cin.tie(NULL);
+ #define vi  vector<int>
+#define pb push_back
+#define f first
+#define s second
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+
+using namespace std;
+#define int long long
+const int inf=1e18;
+const int mod=1e7;
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("outputt.txt", "w", stdout);
+#endif
+  int t; cin>>t;
+  while(t--){
+  	solve();
+  }
+return 0;
+}
